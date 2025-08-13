561e936f4d04bf449ecbd9337c5696b9e207cabb
Nicholas White <106857076+TheRealFalseReality@users.noreply.github.com>
2025-08-12 20:19:44 -0400

    Update react-router.config.ts

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  // Add the basename for GitHub Pages deployment
+  basename: "/aquapi_ai/",
 } satisfies Config;
