5fdf315b54677a615689f3069e35568b67df184f
Nicholas White <106857076+TheRealFalseReality@users.noreply.github.com>
2025-08-13 10:23:10 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/aquapi_ai/' : '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  define: {
+    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY)
+  }
 }));
